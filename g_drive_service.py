2cdad723f583995b127bf748acc6bcee706d2b7a
we-day-memory <dnevnahome@gmail.com>
2024-08-18 16:02:08 +0200

    fic :

--- g_drive_service.py
+++ g_drive_service.py
@@ -1,3 +1,4 @@
+import json
 import os
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
@@ -7,12 +8,20 @@ class GoogleDriveService:
     def __init__(self):
         self._SCOPES=['https://www.googleapis.com/auth/drive']
 
-        _base_path = os.path.dirname(__file__)
-        _credential_path=os.path.join(_base_path, 'credentials.json')
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = _credential_path
+        # _base_path = os.path.dirname(__file__)
+        # _credential_path=os.path.join(_base_path, 'credentials.json')
+        # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = _credential_path
 
     def build(self):
-        creds = ServiceAccountCredentials.from_json_keyfile_name(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"), self._SCOPES)
+        # Load credentials from environment variable
+        credentials_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+        if not credentials_json:
+            raise ValueError("Environment variable GOOGLE_APPLICATION_CREDENTIALS_JSON is not set.")
+
+        # Parse JSON credentials
+        credentials_info = json.loads(credentials_json)
+
+        creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_info, self._SCOPES)
         service = build('drive', 'v3', credentials=creds)
 
         return service
