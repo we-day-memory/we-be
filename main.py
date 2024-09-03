afe35917da35c4e0d62dc9a025bc3fbe84c82c5a
we-day-memory <dnevnahome@gmail.com>
2024-09-03 20:53:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 
 service=GoogleDriveService().build()
 
-ROOT_FOLDER_ID = f'{os.getenv("FOLDER_ID")}'
+ROOT_FOLDER_ID = f'{os.getenv("FOLDER_ID_DD")}'
 
 def get_key():
     file_path = "./secret.key"  # Path to the file on the server
